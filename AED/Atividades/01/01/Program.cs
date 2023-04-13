18b647cb7bd34eb38d28fa189156ffaac18fcd89
Israel Alexsander <81657995+IsraelAlexsander@users.noreply.github.com>
2023-04-13 15:47:58 -0300

    Add files via upload

---
+++ AED/Atividades/01/01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int P1, P2, soma, Quadrado;
+
+            for (int i = 1000; i <= 9999; i++)
+            {
+                P1 = i / 100;
+                P2 = i % 100;
+
+                soma = P1+ P2;
+                Quadrado = soma * soma;
+
+                if (Quadrado == i)
+                    Console.WriteLine($"O número {i} possui essa caracteristica...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
