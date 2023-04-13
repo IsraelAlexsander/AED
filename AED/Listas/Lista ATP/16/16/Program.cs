748a9346997556eca0eb5df486523ad22b547d5e
Israel Alexsander <81657995+IsraelAlexsander@users.noreply.github.com>
2023-04-13 16:28:55 -0300

    Add files via upload

---
+++ AED/Listas/Lista ATP/16/16/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] V = new int[10];
+            int[] R = new int[10];
+            int cont = 0;
+
+            Random x = new Random();
+
+            for (int i = 0; i < V.Length - 1; i++)
+            {
+                V[i] = x.Next(0, 11);
+                Console.Write($"{V[i],4}");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (V[i] == V[j])
+                    {
+                        bool existe = false;
+                        for (int k = j + 1; k < cont; k++)
+                        {                            
+                            if (V[i] == R[k])
+                                existe = true;
+                        }
+
+                        if (existe == false)
+                        {
+                            R[cont] = V[i];
+                            cont++;
+                        }
+                    }
+                }
+            }
+
+            Console.Write("Elementos repetidos no vetor: ");
+            if (cont == 0)
+                Console.WriteLine("Nenhum");
+            else
+            {
+                for (int i = 0; i < cont; i++)
+                {
+                    Console.Write($"{R[i],4}");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ AED/Listas/Lista ATP/18/18/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    internal class Program
+    {
+
+        static int[,] GeraMatriz(int n)
+        {
+            int[,] M = new int[n,n];
+
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    M[i, j] = random.Next(0, 11);
+
+            return (M);
+        }
+
+
+        static void MostraMatriz(int[,] Matriz)
+        {
+            for (int i = 0; i < Matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matriz.GetLength(1); j++)
+                {
+                    Console.Write($"{Matriz[i,j],4}");
+                }
+                Console.WriteLine();
+            } 
+        }
+
+
+        static int[,] MatrizResultante(int[,] M)
+        {
+            int[,] Matriz = new int[M.GetLength(0),M.GetLength(1)];
+
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                for (int j = 0; j < M.GetLength(1); j++)
+                {
+                    if (M[i,j] % 2 == 0)
+                    {
+                        Matriz[i,j] = M[i,j];
+                    }
+                    else
+                    {
+                        Matriz[i, j] = 0;
+                    }
+                }
+            }
+
+            return (Matriz);
+        }
+
+
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Digite a medida da matriz quadrada: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] M = GeraMatriz(n);
+            int[,] R = MatrizResultante(M);
+
+            Console.WriteLine("Matriz Normal");
+            MostraMatriz(M);
+
+            Console.WriteLine();                       
+
+            Console.WriteLine("Matriz Resultante");
+            MostraMatriz(R);
+
+            Console.ReadKey();
+        }
+    }
+}
